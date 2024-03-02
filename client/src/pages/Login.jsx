ad8a553f966e1d380283ce213d98fb34adf647f5
PrateekTechmatys <prateeky562@gmail.com>
2024-03-03 01:22:37 +0530

    login and signup done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,138 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Button, Container, Paper, TextField, Typography } from "@mui/material"
 const Login = () => {
+  const [isLogin, setLogin] = useState(true);
+
+const toggleLogin = ()=> setLogin((prev)=>!prev);
   return (
-    <div>
-      Login
-    </div>
+    <Container 
+     component={"main"} 
+     maxWidth="xs"
+     sx={{
+      height:"100vh",
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center"
+     }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {
+
+          isLogin ? (
+            <>
+              <Typography variant='h5'>Login</Typography>
+              <form style={{
+
+              }}>
+                <TextField
+                  required
+                  fullWidth
+                  label="Username"
+                  margin='normal'
+                  variant='outlined'
+                />
+
+                <TextField
+                  required
+                  fullWidth
+                  type='password'
+                  label="Username"
+                  margin='normal'
+                  variant='outlined'
+                />
+
+                <Button
+                  sx={{
+                    marginTop: "1rem",
+                  }}
+                  variant='contained'
+                  color='primary'
+                  type='submit'
+                  fullWidth
+                >
+                  Login
+                </Button>
+                <Typography textAlign={"center"} m={"1rem"}>
+                  OR
+                </Typography>
+                <Button fullWidth variant='text' onClick={toggleLogin}>
+                  Sign up Instead
+                </Button>
+              </form>
+            </>
+          )
+            :
+            (
+              <>
+               <Typography variant='h5'>SignUp</Typography>
+              <form style={{
+
+              }}>
+                <TextField
+                  required
+                  fullWidth
+                  label="name"
+                  margin='normal'
+                  variant='outlined'
+                />
+                 <TextField
+                  required
+                  fullWidth
+                  label="bio"
+                  margin='normal'
+                  variant='outlined'
+                />
+                 <TextField
+                  required
+                  fullWidth
+                  label="Username"
+                  margin='normal'
+                  variant='outlined'
+                />
+
+                <TextField
+                  required
+                  fullWidth
+                  type='password'
+                  label="Username"
+                  margin='normal'
+                  variant='outlined'
+                />
+
+                <Button
+                  sx={{
+                    marginTop: "1rem",
+                  }}
+                  variant='contained'
+                  color='primary'
+                  type='submit'
+                  fullWidth
+                >
+                  Login
+                </Button>
+                <Typography textAlign={"center"} m={"1rem"}>
+                  OR
+                </Typography>
+                <Button fullWidth variant='text' onClick={toggleLogin}>
+                  Login up Instead
+                </Button>
+              </form>
+            </>
+              
+            )
+
+        }
+      </Paper>
+
+    </Container>
   )
 }
 
