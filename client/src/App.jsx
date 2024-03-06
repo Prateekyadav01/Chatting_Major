1d10b36516b8d3ae40533f24e0b187da6d3a178f
PrateekTechmatys <prateeky562@gmail.com>
2024-03-06 20:25:42 +0530

    Protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,37 @@
-import React, {lazy} from 'react'
-import {BrowserRouter , Routes , Route} from "react-router-dom"
-import Home from './pages/Home'
-import Login from './pages/Login';
-import Chat from './pages/Chat';
-import Groups from './pages/Groups';
+import React, { lazy } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import ProtectedRoute from './components/auth/ProtectedRoute';
 
 
 // dynamic routes upload
-const home = lazy(()=> import("./pages/Home"));
-const login = lazy(()=> import("./pages/Login"));
-const chat = lazy(()=> import("./pages/Chat"));
-const groups = lazy(()=> import('./pages/Groups'));
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import('./pages/Groups'));
 
 
-
+let user = false;
 const App = () => {
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path='/' element={<Home/>}></Route>
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/groups' element={<Groups/>}/>
-    <Route path='/chat/:chatId' element={<Chat/>}/>
-   </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route element={
+          <ProtectedRoute user={user}>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/groups' element={<Groups />} />
+            <Route path='/chat/:chatId' element={<Chat />} />
+          </ProtectedRoute>
+        }>
+        </Route>
+
+        <Route path='/login' element={
+          <ProtectedRoute user={!user}>
+            <Login />
+          </ProtectedRoute>
+        } />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
