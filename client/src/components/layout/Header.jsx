074d49c16c73b0569c7bf11647e1e2ef608806ba
PrateekTechmatys <prateeky562@gmail.com>
2024-03-08 16:50:39 +0530

    navigation of groups

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,19 +1,24 @@
 import { AppBar, Box, IconButton, Toolbar, Tooltip, Typography } from '@mui/material'
 import React from 'react'
 import { orange } from '../constants/color'
-import { Add as AddIcon, Menu as MenuIcon, Search as SearchIcon } from "@mui/icons-material"
+import { Add as AddIcon, Menu as MenuIcon, Search as SearchIcon , Group as GroupIcon} from "@mui/icons-material"
+import {useNavigate} from 'react-router-dom'
+
 
 const Header = () => {
-
+  const navigate = useNavigate();
   const handleMobile = () => {
     console.log('mobile')
   }
 
-  const openSearchDialog = () => { 
+  const openSearchDialog = () => {
     console.log('search')
   }
-  const openNewGroup= ()=>{
+  const openNewGroup = () => {
     console.log('new group')
+  }
+  const NavigationtoGroup = () => {
+    navigate('/groups')
   }
   return (
     <>
@@ -57,25 +62,35 @@ const Header = () => {
 
             <Box>
               <Tooltip title="Search">
-              <IconButton
-                size="large"
-                edge="end"
-                color="inherit"
-                aria-label="menu"
-                onClick={openSearchDialog}
-              >
-                <SearchIcon />
-              </IconButton>
+                <IconButton
+                  size="large"
+                  edge="end"
+                  color="inherit"
+                  aria-label="menu"
+                  onClick={openSearchDialog}
+                >
+                  <SearchIcon />
+                </IconButton>
               </Tooltip>
 
               <Tooltip title="New Group">
-              <IconButton
-               color='inherit'
-               size='large'
-               onClick={openNewGroup}
-               >
-                <AddIcon />
-               </IconButton>
+                <IconButton
+                  color='inherit'
+                  size='large'
+                  onClick={openNewGroup}
+                >
+                  <AddIcon />
+                </IconButton>
+              </Tooltip>
+
+              <Tooltip title="Manage Group">
+                <IconButton
+                  color='inherit'
+                  size='large'
+                  onClick={NavigationtoGroup}
+                >
+                  <GroupIcon />
+                </IconButton>
               </Tooltip>
 
             </Box>
