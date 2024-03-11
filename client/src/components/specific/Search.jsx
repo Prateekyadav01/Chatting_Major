5fb8e402ed3d7c70207548cb7fe3556c4702b9ce
PrateekTechmatys <prateeky562@gmail.com>
2024-03-11 19:01:59 +0530

    adding the Dialog

--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,9 +1,28 @@
-import { Dialog } from '@mui/material'
+import { Dialog, DialogTitle, InputAdornment, Stack, TextField } from '@mui/material'
 import React from 'react'
-
+import {Search as SearchIcon} from '@mui/icons-material'
+import {useInputValidation} from '6pp'
 const Search = () => {
+
+  const search = useInputValidation("");
+
   return <Dialog open >
-    abcd
+    <Stack padding={"2rem"} direction={"column"} width={"25rem"}>
+    <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+    <TextField label="" value={search.value} 
+    onChange={search.changeHandler}
+    variant='outlined'
+    size='small'
+    InputProps={{
+      startAdornment: (
+        <InputAdornment position="start">
+          <SearchIcon />
+        </InputAdornment>
+      )
+    }}
+    />
+
+    </Stack>
   </Dialog>
 }
 
