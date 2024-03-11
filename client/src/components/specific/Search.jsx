002ca6ea5be9f013bd688ce351316ebdb98af365
PrateekTechmatys <prateeky562@gmail.com>
2024-03-11 19:25:54 +0530

    adding the Dialog

--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,26 +1,39 @@
-import { Dialog, DialogTitle, InputAdornment, Stack, TextField } from '@mui/material'
+import { Dialog, DialogTitle, InputAdornment, List, ListItem, ListItemText, Stack, TextField } from '@mui/material'
 import React from 'react'
-import {Search as SearchIcon} from '@mui/icons-material'
-import {useInputValidation} from '6pp'
+import { Search as SearchIcon } from '@mui/icons-material'
+import { useInputValidation } from '6pp'
+
+
+const users =['1', '2','3','4'];
 const Search = () => {
 
   const search = useInputValidation("");
 
   return <Dialog open >
     <Stack padding={"2rem"} direction={"column"} width={"25rem"}>
-    <DialogTitle textAlign={"center"}>Find People</DialogTitle>
-    <TextField label="" value={search.value} 
-    onChange={search.changeHandler}
-    variant='outlined'
-    size='small'
-    InputProps={{
-      startAdornment: (
-        <InputAdornment position="start">
-          <SearchIcon />
-        </InputAdornment>
-      )
-    }}
-    />
+      <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+      <TextField label="" value={search.value}
+        onChange={search.changeHandler}
+        variant='outlined'
+        size='small'
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          )
+        }}
+      />
+
+    <List>
+      {
+        users.map((user) =>(
+          <ListItem >
+            <ListItemText  />
+          </ListItem>
+        ))
+      }
+    </List>
 
     </Stack>
   </Dialog>
