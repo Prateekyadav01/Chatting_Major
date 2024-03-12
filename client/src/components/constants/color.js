47575d48a79d04beb2d7d4d7f27d6660af8f6e12
PrateekTechmatys <prateeky562@gmail.com>
2024-03-12 23:46:38 +0530

    newGroup is made

--- client/src/components/constants/color.js
+++ client/src/components/constants/color.js
@@ -1,1 +1,3 @@
 export const orange = "#ea7070"
+
+export const grayColor = "rgba(255,255,255,0.1)"
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import AppLayout from '../components/layout/AppLayout'
+import { Stack } from '@mui/material';
+import { grayColor } from '../components/constants/color';
 
 const Chat = () => {
+  const containerRef = useRef();
   return (
-    <div>
-      Chat
-    </div>
+    <>
+    <Stack ref={containerRef}
+    height={"80vh"}
+    spacing={"1rem"}
+    padding={"1rem"}
+    boxShadow={"border-box"}
+    bgcolor={"blue"}
+    sx={{
+      overflowX: "hidden",
+      overflowY: "auto",
+      
+    }}
+    >
+chat
+    </Stack>
+    </>
   )
 }
 
